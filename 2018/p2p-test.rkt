78939adbb80a61a39bd1671906be1e8713eb02af
Boro Sitnikovski <buritomath@gmail.com>
2018-07-12 23:42:48 +0300

    Refactor p2p-test code by using prefab structs for easier serialization and implement sync of valid peers

--- 2018/p2p-test.rkt
+++ 2018/p2p-test.rkt
@@ -1,25 +1,33 @@
 #lang racket
 (require racket/serialize)
 
-(define (string-starts-with str1 str2)
-  (let ([str1len (string-length str1)]
-        [str2len (string-length str2)])
-    (equal? str2 (substring str1 0 (min str2len str1len)))))
+; Peer info structure contains an ip and a port
+(struct peer-info (ip port) #:prefab)
 
-(serializable-struct peer-info (ip port) #:transparent)
+; Peer info IO structure additionally contains IO ports
+(struct peer-info-io (pi input-port output-port) #:prefab)
 
-(serializable-struct peer (ip port input-port output-port) #:transparent)
+; Peer context data contains all information needed for a single peer.
+(struct peer-context-data (name ; Name of this peer
+                           port ; Port number to use
+                           [valid-peers #:mutable] ; List of valid peers (will be updated depending on info retrieved from connected peers)
+                           [connected-peers #:mutable] ; List of connected peers (will be a (not necessarily strict) subset of valid-peers)
+                           [latest-hash #:mutable]) ; Latest hash will be updated from data with other peers
+  #:prefab)
 
-(struct peer-context-data (name port valid-peers [connected-peers #:mutable] [latest-hash #:mutable]) #:transparent)
-
-(define (peer-to-peer-info p) (peer-info (peer-ip p) (peer-port p)))
-
+; Handler for updating latest hash
 (define (maybe-update-hash peer-context line)
   (let ([current-hash (string->number (string-replace line #rx"(latest-hash:|[\r\n]+)" ""))]
         [latest-hash (peer-context-data-latest-hash peer-context)])
     (when (and (number? current-hash) (> current-hash latest-hash))
-      (displayln "hash updated")
+      (printf "Hash updated to ~a for peer ~a\n" current-hash (peer-context-data-name peer-context))
       (set-peer-context-data-latest-hash! peer-context current-hash))))
+
+; Handler for updating valid peers
+(define (maybe-update-valid-peers peer-context line)
+  (let ([valid-peers (list->set (deserialize (read (open-input-string (string-replace line #rx"(valid-peers:|[\r\n]+)" "")))))]
+        [current-valid-peers (peer-context-data-valid-peers peer-context)])
+    (set-peer-context-data-valid-peers! peer-context (set-union current-valid-peers valid-peers))))
 
 #| Generic handlers for both client and server |#
 ; Handler
@@ -27,43 +35,41 @@
   (flush-output out)
   (define line (read-line in))
   (when (string? line) ; it can be eof
-    (cond [(string-starts-with line "get-valid-peers")
-           (begin (displayln (serialize (peer-context-data-valid-peers peer-context)) out) (handler peer-context in out))]
-          [(string-starts-with line "get-connected-peers")
-           (begin (displayln (serialize (map peer-to-peer-info (peer-context-data-connected-peers peer-context))) out) (handler peer-context in out))]
-          [(string-starts-with line "get-latest-hash")
+    (cond [(string-prefix? line "get-valid-peers")
+           (begin (display "valid-peers:" out) (displayln (serialize (set->list (peer-context-data-valid-peers peer-context))) out) (handler peer-context in out))]
+          [(string-prefix? line "get-latest-hash")
            (begin (display "latest-hash:" out) (displayln (peer-context-data-latest-hash peer-context) out) (handler peer-context in out))]
-          [(string-starts-with line "latest-hash:")
+          [(string-prefix? line "latest-hash:")
            (begin (maybe-update-hash peer-context line) (handler peer-context in out))]
-          [(string-starts-with line "exit")
+          [(string-prefix? line "valid-peers:")
+           (begin (maybe-update-valid-peers peer-context line) (handler peer-context in out))]
+          [(string-prefix? line "exit")
            (displayln "bye" out)]
           [else (handler peer-context in out)])))
 
 ; Helper to launch handler thread
-(define (launch-handler-thread handler peer-context in out)
-  (define-values (local-ip local-port remote-ip remote-port) (tcp-addresses in #t))
-  (define current-peer (peer remote-ip remote-port in out))
+(define (launch-handler-thread handler peer-context in out cb)
+  (define-values (local-ip remote-ip) (tcp-addresses in))
+  (define current-peer (peer-info remote-ip (peer-context-data-port peer-context)))
+  (define current-peer-io (peer-info-io current-peer in out))
   (thread
    (lambda ()
-     ;TODO
-     (hash-set! (peer-context-data-valid-peers peer-context) (peer-info remote-ip local-port) #t)
-     (set-peer-context-data-connected-peers! peer-context (cons current-peer (peer-context-data-connected-peers peer-context)))
      (handler peer-context in out)
-     (set-peer-context-data-connected-peers! peer-context (set-remove
-                                                        (peer-context-data-connected-peers peer-context)
-                                                        current-peer))
+     (cb)
      (close-input-port in)
      (close-output-port out))))
 
-; Ping all peers in attempt to sync hashes
+; Ping all peers in attempt to sync hashes and update list of valid peers
 (define (peers peer-context)
   (define (loop)
     (sleep 10)
     (for [(p (peer-context-data-connected-peers peer-context))]
-      (let ([in (peer-input-port p)]
-            [out (peer-output-port p)])
+      (let ([in (peer-info-io-input-port p)]
+            [out (peer-info-io-output-port p)])
         (displayln "get-latest-hash" out)
+        (displayln "get-valid-peers" out)
         (flush-output out)))
+    (printf "Peer ~a reports ~a valid peers.\n" (peer-context-data-name peer-context) (set-count (peer-context-data-valid-peers peer-context)))
     (loop))
   (define t (thread loop))
   (lambda ()
@@ -74,7 +80,7 @@
 ; Accept of a new connection
 (define (accept-and-handle listener handler peer-context)
   (define-values (in out) (tcp-accept listener))
-  (launch-handler-thread handler peer-context in out))
+  (launch-handler-thread handler peer-context in out void))
 
 ; Server listener
 (define (serve peer-context)
@@ -95,24 +101,33 @@
   (define conns-cust (make-custodian))
   (parameterize ([current-custodian conns-cust])
     (define (loop)
-      (letrec ([current-connected-ips (map (lambda (p) (peer-info (peer-ip p) (peer-port p))) (peer-context-data-connected-peers peer-context))]
-               [all-valid-peers (hash-keys (peer-context-data-valid-peers peer-context))]
-               [potential-peers (set-subtract all-valid-peers current-connected-ips)])
+      (letrec ([current-connected-peers (list->set (map peer-info-io-pi (peer-context-data-connected-peers peer-context)))]
+               [all-valid-peers (peer-context-data-valid-peers peer-context)]
+               [potential-peers (set-subtract all-valid-peers current-connected-peers)])
         (for ([peer potential-peers])
-          (when #t ; (eq? #f (member ip (list "0" "0.0.0.0" "::1" "127.0.0.1")))
-            ; TODO
-            (thread (lambda ()
-                      (with-handlers
-                          ([exn:fail?
-                            (lambda (x)
-                              ;(displayln x)
-                              ;(printf "Cannot connect to ~a:~a\n" (peer-info-ip peer) (peer-info-port peer))
-                              #t
-                              )])
-                        (begin
-                          ;(printf "Trying to connect to ~a:~a...\n" (peer-info-ip peer) (peer-info-port peer))
-                          (define-values (in out) (tcp-connect (peer-info-ip peer) (peer-info-port peer)))
-                          (launch-handler-thread handler peer-context in out)))))))
+          (thread (lambda ()
+                    (with-handlers
+                        ([exn:fail?
+                          (lambda (x)
+                            ;(printf "Cannot connect to ~a:~a\n" (peer-info-ip peer) (peer-info-port peer))
+                            #t)])
+                      (begin
+                        ;(printf "Trying to connect to ~a:~a...\n" (peer-info-ip peer) (peer-info-port peer))
+                        (define-values (in out) (tcp-connect (peer-info-ip peer) (peer-info-port peer)))
+                        (printf "'~a' connected to ~a:~a!\n" (peer-context-data-name peer-context) (peer-info-ip peer) (peer-info-port peer))
+                        (define current-peer-io (peer-info-io peer in out))
+                        ; Add current peer to list of connected peers
+                        (set-peer-context-data-connected-peers! peer-context (cons current-peer-io (peer-context-data-connected-peers peer-context)))
+                        (launch-handler-thread handler
+                                               peer-context
+                                               in
+                                               out
+                                               (lambda ()
+                                                 ; Remove peer from list of connected peers
+                                                 (set-peer-context-data-connected-peers! peer-context
+                                                                                         (set-remove
+                                                                                          (peer-context-data-connected-peers peer-context)
+                                                                                          current-peer-io)))))))))
         (sleep 10)
         (loop)))
     (thread loop))
@@ -130,10 +145,22 @@
         (stop-peers-loop)
         (stop-listener)))))
 
-(define peer-1-context (peer-context-data "Test peer" 9876 (make-hash) '() (current-milliseconds)))
-(define peer-2-context (peer-context-data "Test peer" 9877 (make-hash (list (cons (peer-info "127.0.0.1" 9876) #t))) '() 1))
+(define peer-1-context (peer-context-data "Test peer 1" 9876 (set (peer-info "127.0.0.1" 9877)) '() (current-milliseconds)))
+(define peer-2-context (peer-context-data "Test peer 2" 9877 (set (peer-info "127.0.0.1" 9876) (peer-info "127.0.0.1" 9878)) '() 1))
+(define peer-3-context (peer-context-data "Test peer 3" 9878 (set (peer-info "127.0.0.1" 9877)) '() 1))
 
 peer-1-context
 peer-2-context
+peer-3-context
+
 (define stop-peer-1 (run-peer peer-1-context))
 (define stop-peer-2 (run-peer peer-2-context))
+(define stop-peer-3 (run-peer peer-3-context))
+
+(define (stop-all-peers)
+  (begin
+    (stop-peer-1)
+    (stop-peer-2)
+    (stop-peer-3)))
+
+(sleep 10000)
