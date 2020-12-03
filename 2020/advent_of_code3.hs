ac39c5c09b520f235acf1d08241df5efd4aaaf59
Boro Sitnikovski <buritomath@gmail.com>
2020-12-03 19:46:45 +0100

    Further cleanup for more elegance

--- 2020/advent_of_code3.hs
+++ 2020/advent_of_code3.hs
@@ -4,37 +4,38 @@ type GridSize   = (Int, Int)
 list :: [String]
 list = []
 
+getGridSize :: [String] -> GridSize
+getGridSize list = (length $ head list, length list)
+
 -- Part one
 findCoordinates :: Coordinate -> GridSize -> [Coordinate]
-findCoordinates (x, y) (gridWidth, gridHeight) = go (x, y) gridHeight where
+findCoordinates (x, y) (width, height) = go (x, y) height where
     go _ 0               = []
-    go (x, y) cnt        = (x `mod` gridWidth, y) : go (slopeModifier (x, y)) (cnt - 1)
+    go (x, y) cnt        = (x `mod` width, y) : go (slopeModifier (x, y)) (cnt - 1)
     slopeModifier (x, y) = (x + 3, y + 1)
 
 countTrees :: [String] -> Int
-countTrees list = let gridWidth    = length $ head list
-                      gridHeight   = length list
-                      coordinates  = findCoordinates (0, 0) (gridWidth, gridHeight)
-                      landedCoords = map (\(x, y) -> list !! y !! x) coordinates
-                  in length $ filter (== '#') landedCoords
+countTrees list =
+    let coordinates  = findCoordinates (0, 0) $ getGridSize list
+        landedCoords = map (\(x, y) -> list !! y !! x) coordinates
+    in length $ filter (== '#') landedCoords
 
 partOne :: Int
 partOne = countTrees list
 
 -- Part two has a more generalized findCoordinates
 findCoordinates' :: Coordinate -> GridSize -> (Coordinate -> Coordinate) -> [Coordinate]
-findCoordinates' (x, y) (gridWidth, gridHeight) slopeModifier = go (x, y) gridHeight where
+findCoordinates' (x, y) (width, height) slopeModifier = go (x, y) height where
     go (x, y) cnt
-        | y > gridHeight = [] -- Need this additional check since we might have a slope modifier that increases height by >1
+        | y > height = [] -- Need this additional check since we might have a slope modifier that increases height by >1
         | cnt == 0       = []
-        | otherwise      = (x `mod` gridWidth, y) : go (slopeModifier (x, y)) (cnt - 1)
+        | otherwise      = (x `mod` width, y) : go (slopeModifier (x, y)) (cnt - 1)
 
 countTrees' :: [String] -> (Coordinate -> Coordinate) -> Int
-countTrees' list slopeModifier = let gridWidth    = length $ head list
-                                     gridHeight   = length list
-                                     coordinates  = findCoordinates' (0, 0) (gridWidth, gridHeight) slopeModifier
-                                     landedCoords = map (\(x, y) -> list !! y !! x) coordinates
-                                 in length $ filter (== '#') landedCoords
+countTrees' list slopeModifier =
+    let coordinates  = findCoordinates' (0, 0) (getGridSize list) slopeModifier
+        landedCoords = map (\(x, y) -> list !! y !! x) coordinates
+    in length $ filter (== '#') landedCoords
 
 slopeModifiers :: [Coordinate -> Coordinate]
 slopeModifiers = [ \(x, y) -> (x + 1, y + 1),
@@ -50,18 +51,17 @@ partTwo :: Int
 partTwo  = product $ map (countTrees' list) slopeModifiers
 
 -- Simpler findCoordinates
-findCoordinates'' :: Coordinate -> GridSize -> (Coordinate -> Coordinate) -> [Coordinate]
-findCoordinates'' (x, y) (gridWidth, gridHeight) slopeModifier = let slopeModifiedCoords = take gridHeight $ iterate slopeModifier (x, y)
-                                                                     flattenedXCoords    = map (\(x, y) -> (x `mod` gridWidth, y)) slopeModifiedCoords
-                                                                     filteredYCoords     = filter (\(x, y) -> y <= gridHeight) flattenedXCoords
-                                                                 in filteredYCoords
+findCoordinates'' :: GridSize -> (Coordinate -> Coordinate) -> [Coordinate]
+findCoordinates'' (width, height) slopeModifier =
+    let slopeModifiedCoords = take height $ iterate slopeModifier (0, 0)
+        flattenedXCoords    = map (\(x, y) -> (x `mod` width, y)) slopeModifiedCoords
+    in filter (\(x, y) -> y <= height) flattenedXCoords -- make sure to include only valid y coords
 
 countTrees'' :: [String] -> (Coordinate -> Coordinate) -> Int
-countTrees'' list slopeModifier = let gridWidth   = length $ head list
-                                      gridHeight   = length list
-                                      coordinates  = findCoordinates'' (0, 0) (gridWidth, gridHeight) slopeModifier
-                                      landedCoords = map (\(x, y) -> list !! y !! x) coordinates
-                                  in length $ filter (== '#') landedCoords
+countTrees'' list slopeModifier =
+    let coordinates  = findCoordinates'' (getGridSize list) slopeModifier
+        landedCoords = map (\(x, y) -> list !! y !! x) coordinates
+    in length $ filter (== '#') landedCoords
 
 partOne'' :: Int
 partOne'' = countTrees'' list ( \(x, y) -> (x + 3, y + 1) )
