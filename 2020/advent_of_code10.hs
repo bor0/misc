1c3f1c162fcf409db14fc08f31fac273dee0e856
Boro Sitnikovski <buritomath@gmail.com>
2020-12-10 16:20:47 +0100

    Add memoization implementation in Haskell

--- 2020/advent_of_code10.hs
+++ 2020/advent_of_code10.hs
@@ -1,6 +1,4 @@
-import Data.IORef
-import System.IO.Unsafe
-
+import Data.Function (fix)
 import Data.List (nub, sort)
 import qualified Data.Map as Map
 import Data.Maybe (isJust, fromJust)
@@ -12,7 +10,7 @@ list2 = [ 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11
 list3 = [ 114, 51, 122, 26, 121, 90, 20, 113, 8, 138, 57, 44, 135, 76, 134, 15, 21, 119, 52, 118, 107, 99, 73, 72, 106, 41, 129, 83, 19, 66, 132, 56, 32, 79, 27, 115, 112, 58, 102, 64, 50, 2, 39, 3, 77, 85, 103, 140, 28, 133, 78, 34, 13, 61, 25, 35, 89, 40, 7, 24, 33, 96, 108, 71, 11, 128, 92, 111, 55, 80, 91, 31, 70, 101, 14, 18, 12, 4, 84, 125, 120, 100, 65, 86, 93, 67, 139, 1, 47, 38 ]
 
 -- Part one
-countAdapterDifferences :: [Integer] -> (Integer, Integer, Integer)
+countAdapterDifferences :: [Int] -> (Int, Int, Int)
 countAdapterDifferences joltages
   | 1 `elem` joltages = go 1 joltages' (1, 0, 0)
   | 2 `elem` joltages = go 2 joltages' (0, 1, 0)
@@ -49,7 +47,19 @@ countAdapterCombinations' joltages = let joltages' = prepare joltages in go (max
     | otherwise = 0
 
 
--- Same solution in Python (countAdapterCombinations')
+memoize :: (Int -> a) -> (Int -> a)
+memoize f = (map f [0 ..] !!)
+
+countAdapterCombinations'' :: [Int] -> Int
+countAdapterCombinations'' joltages = fix (memoize . go joltages') (maximum joltages') where
+    joltages' = 0 : (3 + maximum joltages) : joltages
+    go joltages f 0 = 1
+    go joltages f 1 = 1
+    go joltages f n = (if n - 1 `elem` joltages then f (n - 1) else 0)
+                      + (if n - 2 `elem` joltages then f (n - 2) else 0)
+                      + (if n - 3 `elem` joltages then f (n - 3) else 0)
+
+-- Same solution in Python (countAdapterCombinations'')
 {-
 def prepare_input(input_list):
   l = input_list[:]
