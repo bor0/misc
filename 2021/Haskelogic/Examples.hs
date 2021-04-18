76387785674f983d35bc31300911489ced0f3d8a
Boro Sitnikovski <buritomath@gmail.com>
2021-04-18 12:50:55 +0200

    Improve comments

--- 2021/Haskelogic/Examples.hs
+++ 2021/Haskelogic/Examples.hs
@@ -11,6 +11,7 @@ import Gentzen
   ~~P {outcome}
 ] {pop out of fantasy}
 -}
+-- ⊢ (P)→(¬¬P)
 eg1 = ruleCarryOver ruleDoubleTildeIntro (PropVar P)
 
 {-
@@ -22,6 +23,7 @@ eg1 = ruleCarryOver ruleDoubleTildeIntro (PropVar P)
 ]
 ((P/\Q)->(Q/\P))
 -}
+-- ⊢ ((P)∧(Q))→((Q)∧(P))
 eg2 = ruleCarryOver (\pq -> ruleJoin (ruleSepR pq) (ruleSepL pq)) (And (PropVar P) (PropVar Q))
 
 {-
@@ -36,6 +38,7 @@ eg2 = ruleCarryOver (\pq -> ruleJoin (ruleSepR pq) (ruleSepL pq)) (And (PropVar 
 ]
 (P->(Q->(P/\Q)))
 -}
+-- ⊢ (P)→((Q)→((P)∧(Q)))
 eg3 = ruleCarryOver (\x -> ruleCarryOver (\y -> ruleJoin x y) (PropVar Q)) (PropVar P)
 
 {-
@@ -44,6 +47,7 @@ eg3 = ruleCarryOver (\x -> ruleCarryOver (\y -> ruleJoin x y) (PropVar Q)) (Prop
 (~P -> ~P)
 (P \/ ~P)
 -}
+⊢ (P)∨(¬P)
 eg4 = ruleSwitcheroo $ applyPropRule [GoLeft] ruleDoubleTildeElim $ ruleContra eg1
 
 -- TNT
@@ -57,25 +61,25 @@ eg6 = Not (PropVar (Eq (Plus (S (S Z)) (S (S Z))) (S (S (S Z)))))
 -- if 1 equals to 0, then 0 equals to 1 (wff)
 eg7 = Imp (PropVar (Eq (S Z) Z)) (PropVar (Eq Z (S Z)))
 
--- 0 * 0 = 0
+-- ⊢ ((0)*(0))=(0)
 eg8 = ruleSpec axiom4 A Z
--- forall a.0 * 0 = 0
+-- ⊢ ∀A.((0)*(0))=(0)
 eg8' = ruleGeneralize eg8 A
 
---axiom1 : forall a. not(Sa)=0
---eg9 = exists b.forall a. not(Sa)=B
+--axiom1 : ⊢ ∀A.¬(S(A))=(0)
+-- ⊢ ∃B.∀A.¬(S(A))=(B)
 eg9 = ruleExistenceR axiom1 B [GoLeft,GoLeft] []
---eg9' = exists b.forall a. not(b)=0
+-- ⊢ ∃B.∀A.¬(B)=(0)
 eg9' = ruleExistenceL axiom1 B [GoLeft,GoLeft] []
 
 -- Example proof using induction:
 
 -- Part 1:
--- PropVar (Eq (Plus Z Z) Z)
+-- ⊢ ((0)+(0))=(0)
 egIndBase = ruleSpec (ruleSpec axiom2 B Z) A Z
 
 -- Part 2:
--- PropVar (ForAll A (Imp (PropVar (Eq (Plus Z (Var A)) (Var A))) (PropVar (Eq (Plus Z (S (Var A))) (S (Var A))))))
+-- ⊢ ∀A.(((0)+(A))=(A))→(((0)+(S(A)))=(S(A)))
 egIndHyp_1 = axiom3
 egIndHyp_2 = ruleSpec egIndHyp_1 A Z
 egIndHyp_3 = ruleSpec egIndHyp_2 B (Var A)
@@ -85,10 +89,10 @@ egIndHyp_4 = ruleCarryOver impRule (PropVar (Eq (Plus Z (Var A)) (Var A)))
 egIndHyp_5 = applyFOLRule [GoRight] rule egIndHyp_4
   where
   rule formula = ruleTransitivity egIndHyp_3 formula
-egIndHyp  = ruleGeneralize egIndHyp_5 A
+egIndHyp = ruleGeneralize egIndHyp_5 A
 
--- Proof
-egInd     = ruleInduction egIndBase egIndHyp
+-- ⊢ ∀A.((0)+(A))=(A)
+egInd = ruleInduction egIndBase egIndHyp
 
 {-
 > egInd
@@ -99,17 +103,29 @@ egInd     = ruleInduction egIndBase egIndHyp
 ⊢ ∀A.(A)=((A)+(0))
 -}
 
+-- ⊢ ∀B.((S(0))+(S(B)))=(S((S(0))+(B)))
 step1 = ruleSpec axiom3 A (S Z)
+-- ⊢ ((S(0))+(S(0)))=(S((S(0))+(0)))
 step2 = ruleSpec step1 B Z
+-- ⊢ ((S(0))+(0))=(S(0))
 step1' = ruleSpec axiom2 A (S Z)
+-- ⊢ (S((S(0))+(0)))=(S(S(0)))
 step2' = ruleAddS step1'
+-- ⊢ ((S(0))+(S(0)))=(S(S(0)))
 step3 = ruleTransitivity step2 step2'
 
+-- ⊢ ¬∃A.(S(A))=(0)
 step2_1 = applyFOLRule [] ruleInterchangeL axiom1
+-- ⊢ ¬∃A.(0)=(S(A))
 step2_2 = applyFOLRule [GoRight, GoRight] ruleSymmetry step2_1
+-- ⊢ ¬∃A.¬¬(0)=(S(A))
 step2_3 = applyFOLRule [GoRight, GoRight] ruleDoubleTildeIntro step2_2 -- apply from Gentzen
 
+-- ⊢ ((S(0))=(0))→((0)=(S(0)))
 step3_1 = ruleCarryOver ruleSymmetry (PropVar (Eq (S Z) Z))
+-- ⊢ ((S(S(0)))=(S(0)))→((0)=(S(0)))
 step3_2 = applyFOLRule [GoLeft] ruleAddS step3_1
+-- ⊢ ((0)=(S(0)))→((0)=(S(0)))
 step3_3 = applyFOLRule [GoLeft] ruleSymmetry step3_1
+-- ⊢ ((0)=(S(0)))→((0)=(S(0)))
 step3_4 = applyFOLRule [GoLeft] ruleDropS step3_3
