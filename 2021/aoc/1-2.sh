9ff2a2a387ce2185dff16084465ebf789ca966af
Boro Sitnikovski <buritomath@gmail.com>
2021-12-01 16:33:17 +0100

    Optimize bash solution and add comment to tex one

--- 2021/aoc/1-2.sh
+++ 2021/aoc/1-2.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 IFS=$'\n' read -d '' -r -a lines < input
 len=${#lines[@]}
-sums=()
+increased=0
 for i in $(seq 0 $[len - 2])
   do
     first=${lines[$i]}
     second=${lines[$i+1]}
     third=${lines[$i+2]}
     sum=$[first + second + third]
-    sums+=("${sum}")
-  done
-
-increased=0
-for i in $(seq 1 $[len-1])
-  do
-    prev=${sums[$i-1]}
-    current=${sums[$i]}
-    if [[ $prev -lt $current ]]
-    then
+    if [ -n "$old" ] && [[ $old -lt $sum ]]; then
       increased=$((increased+1))
     fi
+    old=$sum
   done
 
 echo $increased
