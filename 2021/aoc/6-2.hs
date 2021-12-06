23fe2a124df9bf1c57991f83b98a105f3d7231e2
Boro Sitnikovski <buritomath@gmail.com>
2021-12-06 09:28:53 +0100

    Optimize solution

--- 2021/aoc/6-2.hs
+++ 2021/aoc/6-2.hs
@@ -1,38 +1,31 @@
 import System.IO
-import Data.List.Split (splitOn, chunksOf)
+
 import Data.Map (fromListWith, toList, Map)
 import qualified Data.Map as Map
 
-frequency :: (Ord a) => [a] -> [(a, Int)]
-frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
-
 calculate :: Map Int Int -> Map Int Int
 calculate xs =
-  let newMap = Map.mapKeys (\x -> x-1) xs in
-      if not (Map.member (-1) newMap)
-      then newMap
-      else
-        let count   = newMap Map.! (-1)
-            newMap' = Map.delete (-1) newMap
-            newMap'' = Map.insert 6 (count + Map.findWithDefault 0 6 newMap') newMap'
-            newMap''' = Map.insert 8 (count + Map.findWithDefault 0 8 newMap'') newMap''
-        in newMap'''
-
-count :: Int -> [Int] -> Int
-count n xs = Map.foldl (\a b -> a + b) 0 $ start n xs
-
-go :: Int -> Map Int Int -> Map Int Int
-go 0 xs = xs
-go n xs = go (n - 1) (calculate xs)
+  let newMap    = Map.mapKeys (\x -> x - 1) xs
+      count     = Map.findWithDefault 0 (-1) newMap
+      newMap'   = Map.delete (-1) newMap
+      newMap''  = Map.insertWith (+) 6 count newMap'
+      newMap''' = Map.insertWith (+) 8 count newMap''
+      in newMap'''
 
 start :: Int -> [Int] -> Map Int Int
 start n xs = go n $ Map.fromList $ frequency xs
+  where
+  go n xs = iterate calculate xs !! n
+  frequency xs = toList (fromListWith (+) [(x, 1) | x <- xs])
 
+count = Map.foldl (+) 0
+
+-- Parsing and processing
 parseContents contents = read ("[" ++ contents ++ "]") :: [Int]
 
 main = do
   handle <- openFile "input" ReadMode
   contents <- hGetContents handle
   let xs = parseContents contents
-  print $ count 256 xs
+  print $ count (start 256 xs)
   hClose handle
