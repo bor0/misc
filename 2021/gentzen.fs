cd114c7944bfc2cf45f2d7aee0f88ffc9daafb39
Boro Sitnikovski <buritomath@gmail.com>
2021-12-12 00:36:23 +0100

    Add more rules to F#'s Gentzen

--- 2021/gentzen.fs
+++ 2021/gentzen.fs
@@ -4,7 +4,7 @@ type Proof<'a> = Proof of 'a
 
 let fromProof x =
   match x with
-  | (Proof a) -> a
+  | Proof a -> a
 
 type PropCalc<'a> =
   PropVar of 'a
@@ -13,7 +13,31 @@ type PropCalc<'a> =
   | Or of PropCalc<'a> * PropCalc<'a>
   | Imp of PropCalc<'a> * PropCalc<'a>
 
-let ruleJoin (x : Proof<PropCalc<'a>>) (y : Proof<PropCalc<'a>>) = Proof (And (fromProof x, fromProof y))
+let ruleJoin (x : Proof<PropCalc<'a>>) (y : Proof<PropCalc<'a>>)
+  = Proof (And (fromProof x, fromProof y))
+
+let ruleDoubleTildeIntro (x : Proof<PropCalc<'a>>) =
+  match (fromProof x) with
+  | x -> Proof (Not (Not x))
+
+let ruleDoubleTildeElim (x : Proof<PropCalc<'a>>) =
+  match (fromProof x) with
+  | Not (Not x) -> Proof x
+  | _ -> x
+
+let ruleFantasy (x : PropCalc<'a>) (y : Proof<PropCalc<'a>> -> Proof<PropCalc<'a>>)
+  = Proof(Imp(x, fromProof(y (Proof(x)))))
+
+let ruleDetachment (x : Proof<PropCalc<'a>>) (y : Proof<PropCalc<'a>>) =
+  match (fromProof y) with
+  | Imp (x', y) -> if (fromProof x) = x' then Proof y else x
+  | _ -> x
+
+let ruleContra (x : Proof<PropCalc<'a>>) =
+  match (fromProof x) with
+  | Imp (Not y, Not x) -> Proof (Imp (x, y))
+  | Imp (x, y)         -> Proof (Imp (Not x, Not y))
+  | _ -> x
 
 let ruleDeMorgan (x : Proof<PropCalc<'a>>) =
   match (fromProof x) with
@@ -21,12 +45,16 @@ let ruleDeMorgan (x : Proof<PropCalc<'a>>) =
   | Not (Or (x, y))    -> Proof (And (Not x, Not y))
   | x -> Proof x
 
-let ruleFantasy (x : PropCalc<'a>) (y : Proof<PropCalc<'a>> -> Proof<PropCalc<'a>>)
-  = Proof(Imp(x, fromProof(y (Proof(x)))))
+let ruleSwitcheroo (x : Proof<PropCalc<'a>>) =
+  match (fromProof x) with
+  | Or (x, y)    -> Proof (Imp (Not x, y))
+  | Imp (Not x, y) -> Proof (Or (x, y))
+  | x -> Proof x
 
 type Vars = A | B | C
 
 [<EntryPoint>]
 let main argv =
   printf "%A\n" (ruleFantasy (Not (PropVar A)) (fun x -> ruleDeMorgan (ruleJoin x x)))
+  printf "%A\n" (ruleFantasy (Or (PropVar A, PropVar B)) ruleSwitcheroo)
   0 // return an integer exit code
