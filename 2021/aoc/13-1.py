0f1b0d30a844a9c32d6a35fb84315b1c1fef3623
Boro Sitnikovski <buritomath@gmail.com>
2021-12-13 09:44:52 +0100

    Simplify by removing unnecessary lastx/lasty

--- 2021/aoc/13-1.py
+++ 2021/aoc/13-1.py
@@ -6,49 +6,19 @@ with open('input') as f:
   L[1] = L[1].replace('fold along ', '').replace('=','')
   actions = list(filter(lambda x: x, L[1].split('\n')))
 
-#lastx and lasty maintain the coordinate size
-
-def get_lasty(coords, y = None):
-  if y == None:
-    return max(map(lambda (x, y): y, coords)) + 3
-  return y
-
-def add_lasty(coords, y = None):
-  return coords.union(set([(0, get_lasty(coords, y))]))
-
-def discard_lasty(coords):
-  maxy = max(map(lambda (x, y): y, coords))
-  return set(list(filter(lambda (x, y): y != maxy, coords)))
-
 def foldy(coords_up, y):
-  coords_up = discard_lasty(coords_up)
-
   coords_down = set([ p for p in coords_up if p[1] > y ])
   coords_up = coords_up - coords_down
   coords_down = set([ (p[0], 2 * y - p[1]) for p in coords_down if 2 * y - p[1] >= 0 ])
 
-  return add_lasty(coords_up.union(coords_down))
-
-def get_lastx(coords, x = None):
-  if x == None:
-    return max(map(lambda (x, y): x, coords)) + 3
-  return x
-
-def add_lastx(coords, x = None):
-  return coords.union(set([(get_lastx(coords, x), 0)]))
-
-def discard_lastx(coords):
-  maxx = max(map(lambda (x, y): x, coords))
-  return set(list(filter(lambda (x, y): x != maxx, coords)))
+  return coords_up.union(coords_down)
 
 def foldx(coords_left, x):
-  coords_left = discard_lastx(coords_left)
-
   coords_right = set([ p for p in coords_left if p[0] > x ])
   coords_left = coords_left - coords_right
   coords_right = set([ (2 * x - p[0], p[1]) for p in coords_right if 2 * x - p[0] >= 0 ])
 
-  return add_lastx(coords_left.union(coords_right))
+  return coords_left.union(coords_right)
 
 def printaj(coords):
   max_x = max(map(lambda (x, y): x, coords))
@@ -61,9 +31,6 @@ def printaj(coords):
       else: s = s + '.'
     print(s)
 
-coords = add_lasty(coords)
-coords = add_lastx(coords)
-
 actions = [ actions[0] ]
 
 for a in actions:
@@ -72,5 +39,4 @@ for a in actions:
   else:
     coords = foldy(coords, int(a[1:]))
 
-total_hashes = len(coords) - 2 # discard lastx and lasty
-print(total_hashes)
+print(len(coords))
--- 2021/aoc/13-2.py
+++ 2021/aoc/13-2.py
@@ -6,49 +6,19 @@ with open('input') as f:
   L[1] = L[1].replace('fold along ', '').replace('=','')
   actions = list(filter(lambda x: x, L[1].split('\n')))
 
-#lastx and lasty maintain the coordinate size
-
-def get_lasty(coords, y = None):
-  if y == None:
-    return max(map(lambda (x, y): y, coords)) + 3
-  return y
-
-def add_lasty(coords, y = None):
-  return coords.union(set([(0, get_lasty(coords, y))]))
-
-def discard_lasty(coords):
-  maxy = max(map(lambda (x, y): y, coords))
-  return set(list(filter(lambda (x, y): y != maxy, coords)))
-
 def foldy(coords_up, y):
-  coords_up = discard_lasty(coords_up)
-
   coords_down = set([ p for p in coords_up if p[1] > y ])
   coords_up = coords_up - coords_down
   coords_down = set([ (p[0], 2 * y - p[1]) for p in coords_down if 2 * y - p[1] >= 0 ])
 
-  return add_lasty(coords_up.union(coords_down))
-
-def get_lastx(coords, x = None):
-  if x == None:
-    return max(map(lambda (x, y): x, coords)) + 3
-  return x
-
-def add_lastx(coords, x = None):
-  return coords.union(set([(get_lastx(coords, x), 0)]))
-
-def discard_lastx(coords):
-  maxx = max(map(lambda (x, y): x, coords))
-  return set(list(filter(lambda (x, y): x != maxx, coords)))
+  return coords_up.union(coords_down)
 
 def foldx(coords_left, x):
-  coords_left = discard_lastx(coords_left)
-
   coords_right = set([ p for p in coords_left if p[0] > x ])
   coords_left = coords_left - coords_right
   coords_right = set([ (2 * x - p[0], p[1]) for p in coords_right if 2 * x - p[0] >= 0 ])
 
-  return add_lastx(coords_left.union(coords_right))
+  return coords_left.union(coords_right)
 
 def printaj(coords):
   max_x = max(map(lambda (x, y): x, coords))
@@ -61,17 +31,10 @@ def printaj(coords):
       else: s = s + '.'
     print(s)
 
-coords = add_lasty(coords)
-coords = add_lastx(coords)
-
 for a in actions:
   if a[0] == 'x':
     coords = foldx(coords, int(a[1:]))
   else:
     coords = foldy(coords, int(a[1:]))
-
-# For the second part, uncomment this
-coords = discard_lastx(coords)
-coords = discard_lasty(coords)
 
 printaj(coords)
