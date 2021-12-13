f13168f744d2c68feb114132e27177e206ecb5eb
Boro Sitnikovski <buritomath@gmail.com>
2021-12-13 11:51:13 +0100

    Tiny refactors and proper readme filename

--- 2021/aoc/13/13-1.py
+++ 2021/aoc/13/13-1.py
@@ -1,42 +1,31 @@
 with open('input') as f:
   L = f.read().split('\n\n')
-  coords = L[0].split('\n')
-  coords = set(map(lambda x: (int(x.split(',')[0]), int(x.split(',')[1])), coords))
+  points = L[0].split('\n')
+  points = set(map(lambda x: (int(x.split(',')[0]), int(x.split(',')[1])), points))
 
   L[1] = L[1].replace('fold along ', '').replace('=','')
   actions = list(filter(lambda x: x, L[1].split('\n')))
 
-def foldy(coords_up, y):
-  coords_down = set([ p for p in coords_up if p[1] > y ])
-  coords_up = coords_up - coords_down
-  coords_down = set([ (p[0], 2 * y - p[1]) for p in coords_down if 2 * y - p[1] >= 0 ])
+def foldy(points_up, y):
+  points_down = set([ p for p in points_up if p[1] > y ])
+  points_up = points_up - points_down
+  points_down = set([ (p[0], 2 * y - p[1]) for p in points_down if 2 * y - p[1] >= 0 ])
 
-  return coords_up.union(coords_down)
+  return points_up.union(points_down)
 
-def foldx(coords_left, x):
-  coords_right = set([ p for p in coords_left if p[0] > x ])
-  coords_left = coords_left - coords_right
-  coords_right = set([ (2 * x - p[0], p[1]) for p in coords_right if 2 * x - p[0] >= 0 ])
+def foldx(points_left, x):
+  points_right = set([ p for p in points_left if p[0] > x ])
+  points_left = points_left - points_right
+  points_right = set([ (2 * x - p[0], p[1]) for p in points_right if 2 * x - p[0] >= 0 ])
 
-  return coords_left.union(coords_right)
-
-def printaj(coords):
-  max_x = max(map(lambda (x, y): x, coords))
-  max_y = max(map(lambda (x, y): y, coords))
-
-  for j in range(0, max_y+1):
-    s = ''
-    for i in range(0, max_x+1):
-      if (i,j) in coords: s = s + '#'
-      else: s = s + '.'
-    print(s)
+  return points_left.union(points_right)
 
 actions = [ actions[0] ]
 
 for a in actions:
   if a[0] == 'x':
-    coords = foldx(coords, int(a[1:]))
+    points = foldx(points, int(a[1:]))
   else:
-    coords = foldy(coords, int(a[1:]))
+    points = foldy(points, int(a[1:]))
 
-print(len(coords))
+print(len(points))
