45bb34e6574181d6b71165e0c0795f7fd679cc39
Boro Sitnikovski <buritomath@gmail.com>
2021-12-15 12:52:34 +0100

    Refactors

--- 2021/aoc/15/15-1.py
+++ 2021/aoc/15/15-1.py
@@ -7,12 +7,18 @@ leni = len(L)
 lenj = len(L[0])
 
 # Initialize cost
-cost = [ [ float('inf') for a in b*5 ] for b in L*5 ]
+cost = [ [ float('inf') for a in b ] for b in L ]
 cost[0][0] = 0
 
+for i in range(1, leni):
+  cost[i][0] = L[i][0] + cost[i-1][0]
+
+for j in range(1, lenj):
+  cost[0][j] = L[0][j] + cost[0][j-1]
+
 # Process cost
-for i in range(0, leni):
-  for j in range(0, lenj):
+for i in range(1, leni):
+  for j in range(1, lenj):
     if (i, j) == (0, 0): continue
     cost[i][j] = L[i][j] + min(cost[i-1][j], cost[i][j-1])
 
--- 2021/aoc/15/15-2.py
+++ 2021/aoc/15/15-2.py
@@ -1,11 +1,9 @@
-import math
-
 with open('input') as f:
   L = f.read().split('\n')
   L = [ [ int(x) for x in a ] for a in L ]
   L = filter(lambda x: x, L) # empty elements
 
-def get_times5_value(L, i, j, leni, lenj):
+def get_risk_wrap(L, i, j, leni, lenj):
   value = L[i%leni][j%lenj] + int(j/lenj) + int(i/leni)
   if value > 9: value = value % 9
   return value
@@ -17,12 +15,17 @@ lenj = len(L[0])
 cost = [ [ float('inf') for a in b*5 ] for b in L*5 ]
 cost[0][0] = 0
 
+for i in range(1, leni*5):
+  cost[i][0] = get_risk_wrap(L, i, 0, leni, lenj) + cost[i-1][0]
+
+for j in range(1, lenj*5):
+  cost[0][j] = get_risk_wrap(L, 0, j, leni, lenj) + cost[0][j-1]
+
 # Process cost
-for i in range(0, leni*5):
-  for j in range(0, lenj*5):
-    if (i, j) == (0, 0): continue
-    cost[i][j] = get_times5_value(L, i, j, leni, lenj) + min(cost[i-1][j], cost[i][j-1])
+for i in range(1, leni*5):
+  for j in range(1, lenj*5):
+    cost[i][j] = get_risk_wrap(L, i, j, leni, lenj) + min(cost[i-1][j], cost[i][j-1])
 
 print(cost[leni*5-1][lenj*5-1])
 
-#TODO: bonus 9 on the final answer?
+#TODO: extra 9 on the final answer?
