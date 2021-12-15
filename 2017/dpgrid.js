78a90a4a5591077fe6f03171f336ec827d487e80
Boro Sitnikovski <buritomath@gmail.com>
2021-12-15 09:10:18 +0100

    Fix odd spacing

--- 2017/dpgrid.js
+++ 2017/dpgrid.js
@@ -5,15 +5,19 @@ var mat = [
 ];
 
 function calc(mat, i, j) {
-        if (i == 0 && j == 0) {
-                return mat[0][0];                    }
-        if (i == 0) {
-                return mat[i][j] + calc(mat,
-i, j - 1);
-        }                                            if (j == 0) {                                        return mat[i][j] + calc(mat,
-i - 1, j);                                           }                                                                                         return mat[i][j] + Math.max(
-                        calc(mat, i - 1, j),
-                        calc(mat, i, j - 1));
+	if (i == 0 && j == 0) {
+		return mat[0][0];
+	}
+	if (i == 0) {
+			return mat[i][j] + calc(mat, i, j - 1);
+	}
+	if (j == 0) {
+		return mat[i][j] + calc(mat, i - 1, j);
+	}
+
+	return mat[i][j] + Math.min(
+		calc(mat, i - 1, j),
+		calc(mat, i, j - 1));
 }
 
 console.log(calc(mat, 2, 2));
