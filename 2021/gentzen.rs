ce64f38b1945a1fe28df1f1dc4b482639dcd933d
Boro Sitnikovski <buritomath@gmail.com>
2021-12-20 09:01:26 +0100

    Friendlier display

--- 2021/gentzen.rs
+++ 2021/gentzen.rs
@@ -1,24 +1,32 @@
-use std::fmt::Debug;
+use std::fmt;
 use std::clone::Clone;
 use std::convert::identity;
 
-#[derive(Debug, Clone)]
-enum Proof<T:Debug> {
+#[derive(Clone)]
+enum Proof<T: fmt::Debug> {
     Proof(Box<T>),
 }
 
-fn from_proof<T:Debug>(x : Proof<T>) -> T {
+impl<T: fmt::Debug> fmt::Debug for Proof<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Proof::Proof(x) => write!(f, "⊢ {:?}", x),
+        }
+    }
+}
+
+fn from_proof<T: fmt::Debug>(x : Proof<T>) -> T {
     match x {
         Proof::Proof(x) => match *x { x => x },
     }
 }
 
-fn mk_proof<T:Debug>(x : T) -> Proof<T> {
+fn mk_proof<T: fmt::Debug>(x : T) -> Proof<T> {
     Proof::Proof(Box::new(x))
 }
 
-#[derive(Debug, Clone)]
-enum PropCalc<T:Debug> {
+#[derive(Clone)]
+enum PropCalc<T: fmt::Debug> {
     PropVar(Box<T>),
     Not(Box<PropCalc<T>>),
     And(Box<PropCalc<T>>, Box<PropCalc<T>>),
@@ -26,37 +34,49 @@ enum PropCalc<T:Debug> {
     Imp(Box<PropCalc<T>>, Box<PropCalc<T>>),
 }
 
-fn tm_var<T:Debug>(x : T) -> PropCalc<T> {
+impl<T: fmt::Debug> fmt::Debug for PropCalc<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PropCalc::PropVar(x) => write!(f, "{:?}", x),
+            PropCalc::Not(x) => write!(f, "¬({:?})", x),
+            PropCalc::And(x, y) => write!(f, "({:?}∧{:?})", x, y),
+//            PropCalc::Or(x, y) => write!(f, "({:?}∨{:?})", x, y),
+            PropCalc::Imp(x, y) => write!(f, "({:?}→{:?})", x, y),
+        }
+    }
+}
+
+fn tm_var<T: fmt::Debug>(x : T) -> PropCalc<T> {
     PropCalc::PropVar(Box::new(x))
 }
 
-fn tm_not<T:Debug>(x : PropCalc<T>) -> PropCalc<T> {
+fn tm_not<T: fmt::Debug>(x : PropCalc<T>) -> PropCalc<T> {
     PropCalc::Not(Box::new(x))
 }
 
-fn tm_and<T:Debug>(x : PropCalc<T>, y : PropCalc<T>) -> PropCalc<T> {
+fn tm_and<T: fmt::Debug>(x : PropCalc<T>, y : PropCalc<T>) -> PropCalc<T> {
     PropCalc::And(Box::new(x), Box::new(y))
 }
 
-fn tm_imp<T:Debug>(x : PropCalc<T>, y : PropCalc<T>) -> PropCalc<T> {
+fn tm_imp<T: fmt::Debug>(x : PropCalc<T>, y : PropCalc<T>) -> PropCalc<T> {
     PropCalc::Imp(Box::new(x), Box::new(y))
 }
 
-fn rule_fantasy<T:Debug+Clone>(x : PropCalc<T>, y : &dyn Fn(Proof<PropCalc<T>>) -> Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
+fn rule_fantasy<T: fmt::Debug+Clone>(x : PropCalc<T>, y : &dyn Fn(Proof<PropCalc<T>>) -> Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
     let a = x;
     let b = a.clone();
     return mk_proof(tm_imp(a, from_proof(y(mk_proof(b)))));
 }
 
-fn rule_join<T:Debug>(x : Proof<PropCalc<T>>, y : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
+fn rule_join<T: fmt::Debug>(x : Proof<PropCalc<T>>, y : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
     mk_proof(tm_and(from_proof(x), from_proof(y)))
 }
 
-fn rule_double_tilde_intro<T:Debug>(x : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
+fn rule_double_tilde_intro<T: fmt::Debug>(x : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
     mk_proof(tm_not(tm_not(from_proof(x))))
 }
 
-fn rule_double_tilde_elim<T:Debug>(x : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
+fn rule_double_tilde_elim<T: fmt::Debug>(x : Proof<PropCalc<T>>) -> Proof<PropCalc<T>> {
     match from_proof(x) {
         PropCalc::Not(boxed1) => match *boxed1 {
             PropCalc::Not(boxed2) => mk_proof(*boxed2),
@@ -67,8 +87,8 @@ fn rule_double_tilde_elim<T:Debug>(x : Proof<PropCalc<T>>) -> Proof<PropCalc<T>>
 }
 
 fn main() {
-    let prop1_1 = tm_var(1);
-    //let thm1 = rule_fantasy(prop1_1, &|i| -> Proof<PropCalc<i32>> { i });
+    let prop1_1 = tm_var('A');
+    //let thm1 = rule_fantasy(prop1_1, &|i| -> Proof<PropCalc<char>> { i });
     let thm1 = rule_fantasy(prop1_1, &identity);
     println!("{:?}", thm1);
 
@@ -78,9 +98,9 @@ fn main() {
     let thm3 = rule_double_tilde_elim(thm2);
     println!("{:?}", thm3);
 
-    let prop4_1 = tm_var(1);
-    let prop4_2 = tm_var(2);
+    let prop4_1 = tm_var('P');
+    let prop4_2 = tm_var('Q');
     let prop4_3 = tm_and(prop4_1, prop4_2);
-    let thm4 = rule_fantasy(prop4_3, &|i| -> Proof<PropCalc<i32>> { let j = i.clone(); rule_join(i, j) });
+    let thm4 = rule_fantasy(prop4_3, &|i| -> Proof<PropCalc<char>> { let j = i.clone(); rule_join(i, j) });
     println!("{:?}", thm4);
 }
