f6fe9ef099f8ced370d4daa95aba000c4bc0a0fc
Boro Sitnikovski <buritomath@gmail.com>
2022-01-16 00:15:09 +0100

    Add type signatures and a few comments

--- 2022/wordle.hs
+++ 2022/wordle.hs
@@ -7,12 +7,15 @@ import Data.List.Split (splitOn)
 import System.Directory (doesFileExist)
 import System.IO
 
+-- A best starting word is one that does not have any repeated chars and contains as many vowels as possible
+findBest :: [String] -> String
 findBest words = go (filter allCharsUnique words) 0 "" where
   allCharsUnique s = nub s == s
   countVowels s = length $ filter (`elem` "aeiou") s
   go (x:xs) n s = let cnt = countVowels x in if cnt >= n then go xs cnt x else go xs n s
   go [] _ s = s
 
+updateWords :: [String] -> String -> String -> [String]
 updateWords words word dat =
   let processed       = zip3 word (map toUpper dat) [0..length dat]
       getProcessed x  = filter (\(c, d, i) -> d == x) processed
@@ -23,20 +26,10 @@ updateWords words word dat =
       exists          = [ c | (c, d, i) <- processed, d == 'G' || d == 'O' ]
       dark            = [ c | (c, d, i) <- getProcessed 'D' ]
       -- Process dark: Include only those whose letters are also found in `exists`
-      words'''        = [ word | word <- words'', all (\c -> (c `elem` exists) || (c `notElem` word)) dark  ]
+      words'''        = [ word | word <- words'', all (\c -> (c `elem` exists) || (c `notElem` word)) dark ]
   in words'''
 
-main = do
-  fileExists <- doesFileExist "sgb-words.txt"
-  if fileExists then do
-    hSetBuffering stdin LineBuffering -- to be able to use backspace
-    handle <- openFile "sgb-words.txt" ReadMode
-    contents <- hGetContents handle
-    let words = contents `deepseq` filter (/= "") $ splitOn "\n" contents
-    contents `deepseq` hClose handle -- force eval before closing
-    runLoop (findBest words) words
-  else putStrLn "Google for 'sgb-words.txt' and download it in the same cwd as this program."
-
+runLoop :: String -> [String] -> IO ()
 runLoop word words = do
   if length words == 1 then putStrLn $ "Solution: " ++ head words
   else if null words then putStrLn "No solution found."
@@ -53,3 +46,15 @@ runLoop word words = do
   if dat' == "S"
   then runLoop (head filteredWords) filteredWords
   else runLoop (head calculatedWords) calculatedWords
+
+main :: IO ()
+main = do
+  fileExists <- doesFileExist "sgb-words.txt"
+  if fileExists then do
+    hSetBuffering stdin LineBuffering -- to be able to use backspace
+    handle <- openFile "sgb-words.txt" ReadMode
+    contents <- hGetContents handle
+    let words = filter (/= "") $ splitOn "\n" contents
+    contents `deepseq` hClose handle -- force eval before closing
+    runLoop (findBest words) words
+  else putStrLn "Google for 'sgb-words.txt' and download it in the same cwd as this program."
