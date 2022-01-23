f8afa1c4e018a3c398d1e8ab3f4f35f347a66d65
Boro Sitnikovski <buritomath@gmail.com>
2022-01-23 15:26:12 +0100

    Merge Sort full implementation

--- 2022/merge-type.hs
+++ 2022/merge-type.hs
@@ -4,7 +4,8 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-import GHC.TypeLits
+import GHC.TypeLits -- For Nat
+import GHC.TypeNats (Div)
 
 type MergeHelper :: [Nat] -> [Nat] -> Ordering -> [Nat]
 type family MergeHelper xs ys o where
@@ -19,3 +20,37 @@ type family Merge xs ys where
 
 -- > :set -XDataKinds
 -- > :kind! Merge '[1, 3, 5] '[2, 3, 4]
+
+type Take :: Nat -> [Nat] -> [Nat]
+type family Take xs n where
+  Take 0 xs  = '[]
+  Take n '[] = '[]
+  Take n (x:xs) = x : Take (n - 1) xs
+
+-- > :set -XDataKinds
+-- > :kind! Take 3 (Merge '[1, 3, 5] '[2, 3, 4])
+
+type Drop :: Nat -> [Nat] -> [Nat]
+type family Drop xs n where
+  Drop n '[] = '[]
+  Drop 0 xs  = xs
+  Drop n (x:xs) = Drop (n - 1) xs
+
+-- > :set -XDataKinds
+-- > :kind! Drop 3 (Merge '[1, 3, 5] '[2, 3, 4])
+
+type Length :: [Nat] -> Nat
+type family Length xs where
+  Length '[] = 0
+  Length (x:xs) = 1 + Length xs
+
+-- > :set -XDataKinds
+-- > :kind! Length '[1, 2, 3]
+
+type MergeSort :: [Nat] -> [Nat]
+type family MergeSort xs where
+  MergeSort '[x] = '[x]
+  MergeSort xs = Merge (MergeSort (Take (Div (Length xs) 2) xs)) (MergeSort (Drop (Div (Length xs) 2) xs))
+
+-- > :set -XDataKinds
+-- > :kind! Merge '[1, 3, 2, 4]
