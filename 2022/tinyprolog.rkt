550a58edac57bb400c6d1b044e9492d416d579e2
Boro Sitnikovski <buritomath@gmail.com>
2022-02-05 10:00:03 +0100

    Small eq? vs equal? bugfix and implement TODO

--- 2022/tinyprolog.rkt
+++ 2022/tinyprolog.rkt
@@ -2,18 +2,25 @@
 (require racket/hash)
 
 (define (is-variable sym) (string-prefix? (symbol->string sym) "?"))
+(define (merge-hashes? h1 h2)
+  (null? (filter identity (hash-values (hash-intersect h1 h2 #:combine/key (lambda (k v1 v2) (not (equal? v1 v2))))))))
+(define (merge-hashes h1 h2)
+  (hash-union h1 h2 #:combine/key (lambda (k v1 v2) v1)))
+
 (define (unify-single expr1 expr2) (unify-single-iter expr1 expr2 (hash)))
 (define (unify-single-iter expr1 expr2 acc)
   (cond 
     ((or (null? expr1) (null? expr2)) acc) ; return if either list is empty
     ((not (eq? (length expr1) (length expr2))) #f) ; return if not equal length
     ((and (list? (car expr1)) (list? (car expr2))) ; list within list
-     (unify-single-iter (cdr expr1)
-                        (cdr expr2)
-                        (hash-union acc (unify-single-iter (car expr1) (car expr2) acc)))) ; TODO: can same key conflict occur?
+     (unify-single-iter (cdr expr1) (cdr expr2)
+                        (let ([acc-2 (unify-single-iter (car expr1) (car expr2) acc)])
+                          (if (and (hash? acc) (hash? acc-2) (merge-hashes? acc acc-2))
+                              (hash-union acc acc-2 #:combine/key (lambda (k v1 v2) v1))
+                              (hash)))))
     ((equal? (car expr1) (car expr2)) (unify-single-iter (cdr expr1) (cdr expr2) acc)) ; same element keep iterating
     ((and (is-variable (car expr2))
-          (or (not (hash-has-key? acc (car expr2))) (eq? (hash-ref acc (car expr2)) (car expr1)))) ; either var not in hash, or if in hash, same value
+          (or (not (hash-has-key? acc (car expr2))) (equal? (hash-ref acc (car expr2)) (car expr1)))) ; either var not in hash, or if in hash, same value
      (unify-single-iter (cdr expr1) (cdr expr2) (hash-set acc (car expr2) (car expr1)))) ; perform variable replacement
     (else #f)))
 
@@ -36,10 +43,13 @@
 (unify-single '(w (x y) z) '(w (?x y) z)) ; {?x:x}
 (unify-single '(w x y z) '(w ?x ?x z)) ; #f
 (unify-single '(w x x z) '(w ?x ?x z)) ; {?x:x}
+(unify-single '(w (x y) (y y) z) '(w ?x ?x z)) ; #f
+(unify-single '(w (x y) (x y) z) '(w ?x ?x z)) ; {?x:(x y)}
+(unify-single '(w (x y) (x y) z) '(w (?x y) (?x y) z)) ; {?x:x}
+(unify-single '(w (x y) (x y) z) '(w (x ?x) (?x y) z)) ; #f
 
 (unify ctx '(?x ?y programer)) ; boro
 (unify ctx '(?x ?y dete)) ; zaki, kiko
 (unify ctx '(?x ?y ekonomist)) ; diksi
 (unify ctx '(?x sitnikovski ?y)) ; boro kiko
 (unify ctx '(?x sitnikovska ?y)) ; zaki diksi
-
