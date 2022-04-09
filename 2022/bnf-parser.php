d138bc7f17a8c0b2fd60a87b5f338a143845b6bb
Boro Sitnikovski <buritomath@gmail.com>
2022-04-09 13:21:04 +0200

    Update variable names, make code cleaner

--- 2022/bnf-parser.php
+++ 2022/bnf-parser.php
@@ -14,47 +14,49 @@ $grammar = [
 	'name'    => [ 'alpha name', 'alpha' ],
 ];
 
-function rule_match( $grammar, $str, $rule_name, $recurse = false ) {
+function rule_match( $grammar, $expr, $rule_name, $partial = false ) {
 	$rules = $grammar[ $rule_name ];
 	foreach ( $rules as $rule ) {
-		$applied = [];
-		$sstr = explode( ' ', $str );
-		$tokens = explode( ' ', $rule );
-		foreach ( $tokens as $token ) {
-			if ( empty( $sstr ) ) {
-				continue 2;
+		$rules_applied = [];
+		$expr_tok = explode( ' ', $expr );
+		$rule_tok = explode( ' ', $rule );
+		foreach ( $rule_tok as $token ) {
+			// string has been consumed but not all rule rule_tok were rules_applied
+			if ( empty( $expr_tok ) ) {
+				continue 2; // proceed to next rule
 			}
 			if ( ! isset( $grammar[ $token ] ) ) {
 				// treat literal as string since it's not defined in the grammar list
-				if ( $token !== $sstr[0] ) {
-					continue 2;
+				if ( $token !== $expr_tok[0] ) {
+					continue 2; // proceed to next rule
 				} else {
-					array_shift( $sstr );
+					array_shift( $expr_tok );
 				}
 			} else {
 				// treat token as a grammar name
-				[ $ssstr, $aapplied, $valid ] = rule_match( $grammar, implode( ' ', $sstr ), $token, true );
+				[ $new_expr_tok, $new_rules_applied, $valid ] = rule_match( $grammar, implode( ' ', $expr_tok ), $token, true );
 				if ( ! $valid ) {
 					continue 2;
 				}
-				$sstr = $ssstr;
-				$applied = array_merge( $applied, $aapplied );
+				// now that it's valid, use the new_expr_tok which is the remaining of the expr
+				$expr_tok = $new_expr_tok;
+				$rules_applied = array_merge( $rules_applied, $new_rules_applied );
 			}
 		}
-		if ( empty( $sstr ) || $recurse ) {
-			$applied[] = $rule_name;
-			// Either the string is fully parsed, or this is a sub-call (we still have more stuff to parse)
-			return [ $sstr, $applied, true ];
+		if ( empty( $expr_tok ) || $partial ) {
+			$rules_applied[] = $rule_name;
+			// either the string is fully parsed, or this is a sub-call (we still have more stuff to parse)
+			return [ $expr_tok, $rules_applied, true ];
 		}
 	}
 	return [ [], [], false ];
 }
 
-function grammar_match( $grammar, $str ) {
-	foreach ( $grammar as $name => $rule ) {
-		[ $sstr, $applied, $valid ] = rule_match( $grammar, $str, $name );
+function grammar_match( $grammar, $expr ) {
+	foreach ( array_keys( $grammar ) as $rule_name ) {
+		[ , $rules_applied, $valid ] = rule_match( $grammar, $expr, $rule_name );
 		if ( $valid ) {
-			return implode( ',', array_reverse( $applied ) );
+			return implode( ',', array_reverse( $rules_applied ) );
 		}
 	}
 	return false;
