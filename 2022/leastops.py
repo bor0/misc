a3b47b4002892d7c7aafb39872fd356f2f3c02ec
Boro Sitnikovski <buritomath@gmail.com>
2022-11-12 18:49:19 +0100

    Add nonrecursive solution

--- 2022/leastops.py
+++ 2022/leastops.py
@@ -16,3 +16,31 @@ def calc(n):
 
     if incs > decs: return 1 + calc(n+1)
     else: return 1 + calc(n-1)
+
+
+def calc_iter(n):
+    s = 0
+    # base cases
+    while True:
+        if n == 0 or n == 1: return 0+s
+        if n == 3: return 2+s
+
+        # greedily divide by 2
+        if n % 2 == 0:
+            s += 1
+            n//=2
+            continue
+
+        # count number of incs/decs to figure which of n+1 and n-1 has most divisions
+        incs, decs = 0, 0
+        k = n+1
+        while (k//2) % 2 == 0: (incs, k) = (incs+1, k//2)
+        k = n-1
+        while (k//2) % 2 == 0: (decs, k) = (decs+1, k//2)
+
+        if incs > decs:
+            s += 1
+            n = n+1
+        else:
+            s += 1
+            n = n-1
